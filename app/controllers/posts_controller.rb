a9e933d9fd02d0fcb60c2b192ff9f7e24017ab0a
seenmyfate <seenmyfate@gmail.com>
2012-02-24 09:41:21 +0000

    Removed comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
 
@@ -10,8 +8,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
 
@@ -21,8 +17,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # GET /posts/new
-  # GET /posts/new.json
   def new
     @post = Post.new
 
@@ -32,13 +26,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(params[:post])
 
@@ -52,9 +43,7 @@ class PostsController < ApplicationController
       end
     end
   end
-
-  # PUT /posts/1
-  # PUT /posts/1.json
+  
   def update
     @post = Post.find(params[:id])
 
@@ -69,8 +58,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
