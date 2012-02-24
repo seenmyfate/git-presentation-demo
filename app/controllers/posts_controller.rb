f462c9ce07247575855e26cfbf30fc0e4d218f49
seenmyfate <seenmyfate@gmail.com>
2012-02-24 09:44:07 +0000

    Updated controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
   end
 
   def show
     @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @post }
-    end
   end
 
   def new
