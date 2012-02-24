673bec15347f72d6c9e455aed52748cbecb7c83c
seenmyfate <seenmyfate@gmail.com>
2012-02-24 09:35:38 +0000

    Boom

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitDemo::Application.config.secret_token = 'c676a33581da2d07f57b48ab2fe1bb12584e406b8d321cf1a9fc24a05c3d9b3b69cef52d744d05e08da7ff9178306b0a7468ca0a2dce0c06c8d55642c3fba681'
